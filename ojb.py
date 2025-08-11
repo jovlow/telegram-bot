5cc77707929429547f1a0faad7a60d064705806a
Jovan Low <jovvlow@Jovans-MacBook-Air.local>
2025-08-11 09:32:49 +0800

    name change

--- ojb.py
+++ ojb.py
@@ -199,7 +199,8 @@ async def main():
     await app.idle()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
 
 
 #await asyncio.sleep(1) for bot to wait between messages
